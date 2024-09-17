a59df04b946754e4bc430c7ee13e7ee38bd24ff5
kattasenpai31 <thangquoc10052020@gmail.com>
2024-09-17 15:03:02 +0700

    updated

--- Register/login/src/index.js
+++ Register/login/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App1234 />
+    <App/>
   </React.StrictMode>
 );
 
